0b3c76112d7f032a79bd0768880ebbca3dca3c79
BaizetSh <bych7777@gmail.com>
2022-02-05 21:31:06 +0300

    Изменён цвет формы и добавленно обращение на выход

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -36,27 +36,31 @@ namespace WindowsFormsApp1
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(79, 81);
+            this.button1.BackColor = System.Drawing.Color.DarkOrchid;
+            this.button1.ForeColor = System.Drawing.Color.Coral;
+            this.button1.Location = new System.Drawing.Point(12, -12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(212, 114);
+            this.button1.Size = new System.Drawing.Size(279, 428);
             this.button1.TabIndex = 0;
             this.button1.Text = "Отобразить";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(537, 81);
+            this.button2.BackColor = System.Drawing.Color.DarkOrange;
+            this.button2.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.button2.Location = new System.Drawing.Point(537, -12);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(200, 114);
+            this.button2.Size = new System.Drawing.Size(251, 428);
             this.button2.TabIndex = 1;
             this.button2.Text = "Выход";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(368, 127);
+            this.textBox1.Location = new System.Drawing.Point(370, 191);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 2;
@@ -66,12 +70,14 @@ namespace WindowsFormsApp1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
